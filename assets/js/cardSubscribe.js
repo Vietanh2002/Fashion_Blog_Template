a153b84516907810835e351082d527cd26b3222f
Lê Việt Anh <levietanh@Les-MacBook-Air.local>
2021-08-18 23:20:57 +0700

    Release frontend final

---
+++ assets/js/cardSubscribe.js
@@ -0,0 +1,12 @@
+function openCard() {
+    document.getElementById("subscribe").style.display = "block";
+}
+
+function closeCard() {
+    document.getElementById("subscribe").style.display = "none";
+}
+
+function likeFunction(x) {
+    x.style.fontWeight = "blod";
+    x.innerHTML = "&#10003; Liked";
+}
