5df33d3ce4b164030d0773e6549cf5da3cb22179
Lê Việt Anh <levietanh@Les-MacBook-Air.local>
2021-09-05 19:06:54 +0700

    Update 5/9/2021

--- assets/js/cardSubscribe.js
+++ assets/js/cardSubscribe.js
@@ -1,12 +1,30 @@
-function openCard() {
-    document.getElementById("subscribe").style.display = "block";
+// Open card subcribe
+const cardToggle = document.querySelectorAll('.btn__card');
+const card = document.getElementById('subscribe');
+const cardClose = document.querySelector('.close__card');
+
+cardToggle.forEach(n => n.addEventListener('click', () =>{
+    card.classList.add('open');
+}));
+
+if(cardClose) {
+    cardClose.addEventListener('click', () => {
+        card.classList.remove('open');
+    })
 }
 
-function closeCard() {
-    document.getElementById("subscribe").style.display = "none";
+//  Open comment
+document.getElementById('myBtn').click();
+function openComment(id) {
+    let x = document.getElementById(id);
+    if(x.className.indexOf('show') == -1) {
+        x.classList.add('show');
+    } else {
+        x.classList.remove('show');
+    }
 }
 
 function likeFunction(x) {
-    x.style.fontWeight = "blod";
+    x.style.fontWeight = "bold";
     x.innerHTML = "&#10003; Liked";
 }
